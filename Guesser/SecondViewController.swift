7a3e43c2bfb769f8332088329a95fffca57e4d91
Jupally,Hari Priya <s533990@ol10019m.nwmissouri.edu>
2019-02-26 19:30:58 -0600

    Initial Commit

---
+++ Guesser/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Guesser
+//
+//  Created by Jupally,Hari Priya on 2/26/19.
+//  Copyright © 2019 Jupally,Hari Priya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
